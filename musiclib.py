004cb3eed98e83b74eac53d3d869903a32c38bdb
Ayuga01 <168949403+Ayuga01@users.noreply.github.com>
2025-02-14 20:44:34 +0530

    musiclib.py

--- musiclib.py
+++ musiclib.py
@@ -3,4 +3,5 @@ music = {
     "smile":"https://www.youtube.com/watch?v=zgaCZOQCpp8",
     "eyes":"https://www.youtube.com/watch?v=_-YjO6KfEMk",
     "sweater":"https://www.youtube.com/watch?v=UDVLn4Xvyz8"
+    #add any music links you like
 }
